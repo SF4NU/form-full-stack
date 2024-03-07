6ab5242fb1092678b209a46853be5145d9411a51
SF4NU <mariusmarius200121@gmail.com>
2024-03-07 17:36:41 +0100

    modificato il nome del repository quindi anche di alcune cose all'interno dei file

--- react-node-client/vite.config.js
+++ react-node-client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-node-prova/",
+  base: "/form-full-stack/",
   plugins: [react()],
 })
